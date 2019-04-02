ff58961c86408e708b92767d5a644de372511ed2
Kevin Garvey and Marissa Perry <kevin@>
2019-04-02 14:08:29 -0700

    add if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
 $(document).ready(function(){
   $("form#luck_quiz").submit(function(event){
     event.preventDefault();
-    $("#luck-responses").show();
-    var totalbad $("input:checkbox[name=bad-luck]:checked").length;
-    alert(totalBad);
-    $("input:checkbox[name=bad-luck]:checked").each(function(){
-      var badLuckTotal = $(this).val();
-    $("input:checkbox[name=good-luck]:checked").each(function(){
-      var goodLuckTotal = $(this).val();
+    // var totalbad = $("input:checkbox[name=bad-luck]:checked").length;
+    // alert(totalBad);
+    // var totalGood = $("input:checkbox[name=bad-luck]:checked").length;
+    // alert(totalGood);
 
-
-
-      $('#luck-responses').append( + "<br>");
-    });
-    // $("#fun-responses").show();
-    // $("input:checkbox[name=good-luck]:checked").each(function(){
-    //   var goodLuckTotal = $(this).val();
-    //   $('#fun-responses').append(funTransportationMode + "<br>");
+    // $("input:checkbox[name=bad-luck]:checked").each(function(){
+    // badLuckTotal = parseInt($(this).val());
+var badLuckTotal = ($("input:checkbox[name=bad-luck]:checked").length);
+var goodLuckTotal = ($("input:checkbox[name=good-luck]:checked").length);
+alert(badLuckTotal)
+alert(goodLuckTotal)
+alert(badLuckTotal + goodLuckTotal)
+    //
     // });
-    $('#luck_quiz').hide();
+    // $("input:checkbox[name=good-luck]:checked").each(function(){
+    //   goodLuckTotal = parseInt($(this).val());
+    // });
+
+
+    if (badLuckTotal < goodLuckTotal) {
+      $('#luck-responses').append("Good fortune");
+    } else if (badLuckTotal === goodLuckTotal) {
+      $('#luck-responses').append("neutral fortune");
+    } else if (badLuckTotal > goodLuckTotal) {
+      $('#luck-responses').append("bad fortune");
+    }
+    //
+    //
+    //
+    //   $('#luck-responses').append( + "<br>");
+    // });
+    // // $("#fun-responses").show();
+    // // $("input:checkbox[name=good-luck]:checked").each(function(){
+    // //   var goodLuckTotal = $(this).val();
+    // //   $('#fun-responses').append(funTransportationMode + "<br>");
+    // // });
+    // $('#luck_quiz').hide();
   });
 });
